246a58334b53918d4adb5ab371912212d77d40cd
steven ponce <steven_ponce@yahoo.com>
2022-03-03 06:59:36 -0500

    update 02_data_wrangling module

--- R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/02_data_wrangling/04_text.R
+++ R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/02_data_wrangling/04_text.R
@@ -38,51 +38,84 @@ bikes_tbl %>%
 # 1.2 Case & Concatenation ----
 
 # Case
-bishop_name <- 'Ithaca Mountain Climbers'
+bikeshop_name <- 'Ithaca Mountain Climbers'
 
-str_to_lower(bishop_name)
-str_to_upper(bishop_name)
-str_to_title(bishop_name)
+str_to_lower(bikeshop_name)
+str_to_upper(bikeshop_name)
+str_to_title(bikeshop_name)
 
 
 # Concatenation
+
+# Vector
 order_id <- 1
 order_line <- 1
 
 # sentence
 str_c('Order Line: ', order_id, '.' , order_line,
-      'sent to Customer: ', bishop_name)
+      ' sent to Customer: ', bikeshop_name)
 
-
-
-
-# Vector
+str_glue("Order Line: {order_id}.{order_line} sent to Customer: {str_to_upper(bikeshop_name)}")
 
 
 
 # Tibble
-
+bike_orderlines_tbl %>% 
+    select(bikeshop_name, order_id, order_line) %>% 
+    mutate(
+        purchase_statement = str_glue(
+            "Order Line: {order_id}.{order_line} sent to Customer: {str_to_upper(bikeshop_name)}") %>% 
+            as.character()
+    )
 
 # 1.3 Separating Text: See tidyr::separate() ----
 
 # Vector
+c("Road - Elite Road - Carbon - Road - Elite Road") %>% 
+    str_split(pattern = ' - ')
+
+
+# better - return as a matrix
+c("Road - Elite Road - Carbon - Road - Elite Road") %>% 
+    str_split(pattern = ' - ', simplify = TRUE)
 
 
 # Tibble
-
+# much better option
+bikes_tbl %>% 
+    select(description) %>% 
+    separate(col    = description, 
+             into   = c('category_1', 'category_2', 'frame_material'),
+             sep    = ' - ',
+             remove = FALSE)
 
 
 # 1.4 Trimming Text ----
 
+'  text with spaces   ' %>%  str_trim(side = 'both')
+'  text with spaces   ' %>%  str_trim(side = 'right')
+'  text with spaces   ' %>%  str_trim(side = 'left')
 
 
 # 1.5 Replacement: Used with mutate() [and optionally case_when()] ----
 
 # Vector
+c('CAAD12', 'CAAD', 'CAAD8') %>% 
+    # replace all numbers with ''
+    str_replace(pattern     = '[0-9]+',
+                replacement = '')
 
 
 # Tibble
-
+bikes_tbl %>% 
+    select(model) %>% 
+    mutate(
+        model_num_removed = str_replace(string      = model,
+                                        pattern     = '[0-9]+',
+                                        replacement = '') %>% 
+            str_trim()
+    ) 
+    
 
 
 
