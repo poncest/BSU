e30d6847a34ba4a7de84737613995f27f8ac5b26
steven ponce <steven_ponce@yahoo.com>
2022-03-10 07:19:24 -0500

    update 03_visualization module

--- R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/03_visualization/02_ggplot2_geometries.R
+++ R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/03_visualization/02_ggplot2_geometries.R
@@ -82,10 +82,26 @@ revenue_month_tbl %>%
 
 # Data Manipulation
 
+revenue_by_cat2_tbl <- bike_orderlines_tbl %>% 
+    
+    select(category_2, total_price) %>% 
+    group_by(category_2) %>% 
+    summarise(revenue = sum(total_price)) %>% 
+    ungroup()
+
 
 # Bar Plot
-
-
+revenue_by_cat2_tbl %>% 
+    # sort by revenue
+    mutate(category_2 = category_2 %>%  as_factor() %>% 
+               fct_reorder(revenue)) %>% 
+    
+    ggplot(aes(x = category_2,
+               y = revenue)) +
+    
+    geom_col()+
+    coord_flip()+
+    theme_classic()
 
 
 
@@ -93,17 +109,36 @@ revenue_month_tbl %>%
 # 4.0 Histogram / Density Plots ----
 # - Great for inspecting the distribution of a variable
 
-
 # Goal: Unit price of bicycles
 # Histogram
 
+bike_orderlines_tbl %>% 
+    select(model, price) %>% 
+    ggplot(aes(x = price)) +
+    geom_histogram(bins = 20, fill = '#5c9f92', color = 'snow')+
+    theme_classic() 
 
-# Goal: Unit price of bicylce, segmenting by frame material
+# Goal: Unit price of bicycle, segmenting by frame material
+
 # Histogram
+bike_orderlines_tbl %>% 
+    distinct(model, frame_material, price) %>% 
+    ggplot(aes(x = price, fill = frame_material)) +
+    geom_histogram(bins = 20)+
+    facet_wrap(~ frame_material, ncol = 1) +
+    scale_fill_tq()+
+    theme_tq()
 
 
-# Density
-
+# Density 
+bike_orderlines_tbl %>% 
+    distinct(model, frame_material, price) %>% 
+    ggplot(aes(x = price, fill = frame_material)) +
+    geom_density(alpha = 0.5)+
+    #facet_wrap(~ frame_material, ncol = 1) +
+    scale_fill_tq()+
+    theme_tq() + 
+    theme(legend.position = 'bottom')
 
 
 
