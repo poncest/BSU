40fa68ade79564504a69ebb265e17119e4454975
steven ponce <steven_ponce@yahoo.com>
2022-03-13 11:26:05 -0400

    update 03_visualization module

--- R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/03_visualization/04_ggplot2_advanced.R
+++ R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/03_visualization/04_ggplot2_advanced.R
@@ -21,11 +21,93 @@ n <- 10
 
 # Data Manipulation
 
+top_customers_tbl <- bike_orderlines_tbl %>% 
+    select(bikeshop_name, total_price) %>% 
+    
+    mutate(
+        bikeshop_name = as.factor(bikeshop_name) %>% 
+            # Lump together factor levels into "other"
+            fct_lump(n = n, w = total_price)) %>% 
+            
+    group_by(bikeshop_name) %>%
+    
+    summarise(revenue = sum(total_price)) %>% 
+    
+    ungroup() %>% 
+    
+    # reorder by revenue and push 'other' at the bottom
+    mutate(
+        bikeshop_name = bikeshop_name %>% fct_reorder(revenue),
+        bikeshop_name = bikeshop_name %>% fct_relevel('Other', after = 0)) %>% 
+    
+    arrange(desc(bikeshop_name)) %>% 
+    
+    # revenue text
+    mutate(revenue_text = scales::dollar(revenue, scale = 1e-6, suffix = 'M')) %>% 
+    
+    # cumulative percent
+    mutate(
+        cum_pct = cumsum(revenue / sum(revenue)),
+        cum_pct_text = scales::percent(cum_pct)
+           ) %>% 
+    
+    # rank
+    mutate(rank = row_number()) %>%
+    # replacing no. 11 of `other` with NA
+    mutate(rank = case_when(
+        rank == max(rank) ~ NA_integer_,
+        TRUE ~ rank
+    )) %>% 
+    
+    # label text
+    mutate(label_text = str_glue('Rank: {rank}\nRev: {revenue_text}\nCumPct: {cum_pct_text}'))
+            
 
 # Data Visualization
 
-
-
+top_customers_tbl %>% 
+    ggplot(aes(x = revenue, y = bikeshop_name)) +
+    
+    # geometries
+    geom_segment(aes(xend = 0, yend = bikeshop_name),
+                 color = palette_light()[1],
+                 size = 1) +
+    
+    geom_point(aes(size = revenue),
+               color = palette_light()[1]) +
+    
+    geom_label(aes(label = label_text),
+               hjust = 'inward',
+               size = 3,
+               color = palette_light()[1]) + 
+    
+    # scales
+    scale_x_continuous(labels = scales::dollar_format(scale = 1e-6, suffix = 'M')) %>% 
+    
+    # labels
+    labs(
+        title = str_glue('Top {n} Customers'),
+        # start and end dates
+        subtitle = str_glue(
+            'Start: {year(min(bike_orderlines_tbl$order_date))}
+            End: {year(max(bike_orderlines_tbl$order_date))}'),
+        x = 'Revenue ($M)',
+        y = 'Customer',
+        caption = str_glue(
+            'Top 6 customer contribute
+            51% of purchasing power.')
+        ) + 
+    
+    # theme
+    theme_tq() + 
+    theme(
+        legend.position = 'none',
+        plot.title = element_text(face = 'bold'),
+        plot.caption = element_text(face = 'bold.italic')
+    )
+                             
+                            
+  
 
 
 # 2.0 Heatmaps ----
