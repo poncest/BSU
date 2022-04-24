720d6399bf60c83ed532d8797a50d9a0df74f068
steven ponce <steven_ponce@yahoo.com>
2022-04-24 13:54:37 -0400

    06_communication

--- R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/06_communication/02_interactive_plotting_plotly.R
+++ R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/06_communication/02_interactive_plotting_plotly.R
@@ -24,7 +24,7 @@ bike_orderlines_tbl
 # 1.0 TOTAL SALES BY MONTH ----
 
 # 1.1 Preparing Time Series Data ----
-bike_orderlines_tbl %>% 
+total_sales_m_tbl <- bike_orderlines_tbl %>% 
     
     select(order_date, total_price) %>% 
     
@@ -36,35 +36,118 @@ bike_orderlines_tbl %>%
     ungroup() %>% 
     
     # text labels
-    mutate(label_text = str_glue('Sales: {scales::dollar(total_sales)},
-                                 Date: {}'))
+    mutate(label_text = str_glue("Sales: {scales::dollar(total_sales)},
+                                 Date: {date_rounded %>% format('%B %Y')}"))
 
+total_sales_m_tbl
 
 # Formatting Dates
 # - strftime: https://devhints.io/strftime
 
+'2011-01-07 00:00:00' %>% 
+    as_datetime() %>% 
+    format('%B %Y') 
 
+?format
 
 # 1.2 Interactive Plot ----
 
 # Step 1: Create ggplot with text feature
 
+# Static Plot
+g1 <- total_sales_m_tbl %>% 
+    ggplot(aes(x = date_rounded, y = total_sales)) +
+    
+    # geoms
+    geom_point(aes(text = label_text), color = '#2C3E50') +
+    geom_smooth(method = 'loess', span = 0.2) +
+    
+    # format
+    theme_tq() +
+    scale_y_continuous(labels = scales::dollar_format()) +
+    # always show y-axis at 0 for timeseries
+    expand_limits(y = 0) +
+    
+    # labels
+    labs(
+        title = 'Total Sales',
+        x = '',
+        y = 'Revenue (USD)'
+    )
+
+g1
 
 # Step 2: Use ggplotly()
 
-
+# interactive
+ggplotly(g1, tooltip = 'text')
 
 
 # 1.3 Plot Total Sales Function ----
 
+plot_total_sales <- function(unit = 'weekly', date_format = '%B %Y', interactive = TRUE) {
+    
+    # handle data
+    data_tbl <- bike_orderlines_tbl %>% 
+        
+        select(order_date, total_price) %>% 
+        
+        # round down the month
+        mutate(date_rounded = floor_date(order_date, unit = unit)) %>% 
+        
+        group_by(date_rounded) %>% 
+        summarise(total_sales = sum(total_price)) %>% 
+        ungroup() %>% 
+        
+        # text labels
+        mutate(label_text = str_glue("Sales: {scales::dollar(total_sales)},
+                                 Date: {date_rounded %>% format(date_format)}"))
+    
+    
+    # make the plot
+    g1 <- data_tbl %>% 
+        ggplot(aes(x = date_rounded, y = total_sales)) +
+        
+        # geoms
+        geom_point(aes(text = label_text), color = '#2C3E50') +
+        geom_smooth(method = 'loess', span = 0.2) +
+        
+        # format
+        theme_tq() +
+        scale_y_continuous(labels = scales::dollar_format()) +
+        # always show y-axis at 0 for timeseries
+        expand_limits(y = 0) +
+        
+        # labels
+        labs(
+            title = 'Total Sales',
+            x = '',
+            y = 'Revenue (USD)'
+        )
+    
+    
+    # static vs interactive logic
+    if (interactive) {
+        return(ggplotly(g1, tooltip = 'text'))  # interactive
+    } else{
+       return(g1)                               # static
+    } 
+    
+} 
+
+plot_total_sales(unit = 'week', date_format = '%B %d, %Y', interactive = TRUE)
 
 
 # 1.4 Test Our Function ----
+plot_total_sales(unit = 'day', date_format = '%B %d, %Y', interactive = TRUE)
 
+plot_total_sales(unit = 'week', date_format = '%B %d, %Y', interactive = TRUE)
 
+plot_total_sales(unit = 'month', date_format = '%B %d, %Y', interactive = TRUE)
 
+plot_total_sales(unit = 'quarter', date_format = '%B %d, %Y', interactive = TRUE)
 
-
+plot_total_sales(unit = 'year', date_format = '%B %d, %Y', interactive = TRUE)
 
 # 2.0 CATEGORY 2 SALES BY MONTH ----
 
