a6e64d6a70a0822e6200d580a806ecaceaae68f9
steven ponce <steven_ponce@yahoo.com>
2022-05-08 17:56:13 -0400

    project_02_customer_segmentation

--- R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/project_02_customer_segmentation/01_kmeans_clustering.R
+++ R-Track/Course 1 - DS for Business Part 1/DS4B_101_R_Business_Analysis/project_02_customer_segmentation/01_kmeans_clustering.R
@@ -17,67 +17,71 @@ bike_orderlines_tbl <- read_rds("./R-Track/Course 1 - DS for Business Part 1/DS4
 plot_customer_heatmap <- function(interactive = TRUE) {
     
     # DATA MANIPULATION
-    pct_sales_by_customer_tbl <- bike_orderlines_tbl %>% 
+    pct_sales_by_customer_tbl <- bike_orderlines_tbl %>%
         
-        select(bikeshop_name, category_1, category_2, quantity) %>% 
+        select(bikeshop_name, category_1, category_2, quantity) %>%
         
-        group_by(bikeshop_name, category_1, category_2) %>% 
-        summarise(total_qty = sum(quantity)) %>% 
-        ungroup() %>% 
+        group_by(bikeshop_name, category_1, category_2) %>%
+        summarise(total_qty = sum(quantity)) %>%
+        ungroup() %>%
         
-        group_by(bikeshop_name) %>% 
-        mutate(pct = total_qty / sum(total_qty)) %>% 
-        ungroup() %>% 
+        group_by(bikeshop_name) %>%
+        mutate(pct = total_qty / sum(total_qty)) %>%
+        ungroup() %>%
         
-        # factor alphabetically
-        mutate(bikeshop_name = as.factor(bikeshop_name) %>%  fct_rev()) %>% 
+        mutate(bikeshop_name = as.factor(bikeshop_name) %>% fct_rev()) %>%
         
         mutate(label_text = str_glue("Customer: {bikeshop_name}
                                      Category: {category_1}
-                                     Subcategory: {category_2}
+                                     Sub-Category: {category_2}
                                      Quantity Purchased: {total_qty}
-                                     Percent of Sales: {scales::percent(pct)}"))
+                                     Percent of Sales: {scales::percent(pct, accuracy = 0.1)}"))
     
     # VISUALIZATION
-    pct_sales_by_customer_tbl %>% 
+    g <- pct_sales_by_customer_tbl %>%
+        ggplot(aes(category_2, bikeshop_name)) +
         
-        ggplot(aes(x = category_2, y = bikeshop_name)) +
-        
-        # geoms
-        geom_tile(aes(fill = pct)) + 
-        
+        # Geoms
+        geom_tile(aes(fill = pct)) +
         geom_text(aes(label = scales::percent(pct, accuracy = 0.1),
-                      size = 3)) +
+                      text = label_text),
+                  size = 3) +
+        facet_wrap(~ category_1, scales = "free_x") +
         
-        facet_wrap(~ category_1, scales = 'free_x') +
-        
-        # format
-        scale_fill_gradient(low = 'white', high = '#2C3e50') + 
-        
-        # theme
+        # Formatting
+        scale_fill_gradient(low = "white", high = "#2c3e50") +
         theme_tq() +
         theme(
-            axis.text.x     = element_text(angle = 45, hjust = 1),
-            legend.position = 'none',
-            plot.title      = element_text(face = 'bold', size = 20),
-            strip.text.x    = element_text(margin = margin(10,10,10,10,  unit = 'pt'),
-                                           size = 12)
+            axis.text.x = element_text(angle = 45, hjust = 1),
+            legend.position = "none",
+            plot.title = element_text(face = "bold"),
+            strip.text.x = element_text(margin = margin(5, 5, 5, 5, unit = "pt"))
         ) +
-        
-        # labs
-        labs(
-            title = 'Heatmap of Purchasing Habit'
-        )
-        
+        labs(title = "Heatmap of Purchasing Habits")
     
- 
     # INTERACTIVE VS STATIC
-    
+    if (interactive) {
+        
+        g <- g +
+            labs(x = "", y = "") 
+        
+        return(ggplotly(g, tooltip = "text"))
+        
+    } else {
+        
+        g <- g +
+            labs(x = "Bike Type (Category 2)", y = "Customer")
+        
+        return(g)
+        
+    }
     
 }
 
-plot_customer_heatmap()
-plot_customer_heatmap(interactive = FALSE)
+# testing function
+plot_customer_heatmap(interactive = TRUE)   # interactive
+plot_customer_heatmap(interactive = FALSE)  # static
+
 
 # 3.0 CUSTOMER SEGMENTATION PLOT ----
 get_customer_segments <- function(k = 4, seed = 123) {
