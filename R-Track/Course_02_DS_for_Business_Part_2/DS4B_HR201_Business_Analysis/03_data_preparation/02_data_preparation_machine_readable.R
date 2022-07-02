2b2e25ec28f492b8c0eed0ce68df051ceb7cca33
steven ponce <steven_ponce@yahoo.com>
2022-07-02 06:46:42 -0400

    03_data_preparation

--- R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/03_data_preparation/02_data_preparation_machine_readable.R
+++ R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/03_data_preparation/02_data_preparation_machine_readable.R
@@ -307,16 +307,43 @@ glimpse(test_tbl)
 
 # Correlation Analysis ----
 
-get_cor <- function(data, target, use = 'paiwise.complete.obs',
-                    fct_reorder =FALSE, fct_rev = FALSE) {
+get_cor <- function(data, target, use = "pairwise.complete.obs",
+                    fct_reorder = FALSE, fct_rev = FALSE) {
     
     feature_expr <- enquo(target)
     feature_name <- quo_name(feature_expr)
     
     data_cor <- data %>% 
         mutate_if(is.character, as.factor) %>% 
-        mutate_if(is.factor, as.numeric)
-    ....
+        mutate_if(is.factor, as.numeric) %>% 
+        cor(use = use) %>% 
+        as.tibble() %>% 
+        mutate(feature = names(.)) %>% 
+        select(feature, !! feature_expr) %>% 
+        filter(!(feature == feature_name)) %>% 
+        mutate_if(is.character, as_factor)
+    
+    if(fct_reorder) {
+        data_cor <- data_cor %>% 
+            mutate(feature = fct_reorder(feature, !! feature_expr)) %>% 
+            arrange(feature)
+        }
+    
+    if(fct_rev) {
+        data_cor <- data_cor %>% 
+            mutate(feature = fct_rev(feature)) %>% 
+            arrange(feature)
+    }
+    
+    return(data_cor)
+    
 } 
+
+# testing 
+train_tbl %>% 
+    get_cor(target = Attrition_Yes, fct_reorder = T, fct_rev = T)
+
+
+
 
 
