98330fdc15901615ba05a55b57b2eea208cae7dd
steven ponce <steven_ponce@yahoo.com>
2022-08-11 06:49:50 -0400

    04_modeling

--- R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/04_modeling/03_modeling_lime_feature_explanation.R
+++ R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/04_modeling/03_modeling_lime_feature_explanation.R
@@ -4,36 +4,53 @@
 
 # Load Libraries 
 
-library(h2o)
-library(recipes)
-library(readxl)
-library(tidyverse)
-library(tidyquant)
-library(lime)
+library(pacman)
+p_load(h2o, recipes, readxl, tidyverse, tidyquant, lime)
+
 
 # Load Data
-path_train            <- "00_Data/telco_train.xlsx"
-path_test             <- "00_Data/telco_test.xlsx"
-path_data_definitions <- "00_Data/telco_data_definitions.xlsx"
+path_train            <- (here::here('./R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_data/telco_train.xlsx'))
+path_test             <- (here::here('./R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_data/telco_test.xlsx'))
+path_data_definitions <- (here::here('./R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_data/telco_data_definitions.xlsx'))
 
-train_raw_tbl       <- read_excel(path_train, sheet = 1)
-test_raw_tbl        <- read_excel(path_test, sheet = 1)
-definitions_raw_tbl <- read_excel(path_data_definitions, sheet = 1, col_names = FALSE)
+
+train_raw_tbl         <- read_excel(path_train, sheet = 1)
+test_raw_tbl          <- read_excel(path_test, sheet = 1)
+definitions_raw_tbl   <- read_excel(path_data_definitions, sheet = 1, col_names = FALSE)
+
 
 # Processing Pipeline
-source("00_Scripts/data_processing_pipeline_rev1.R")
+source(here::here('R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_scripts/data_processing_pipeline_rev1.R'))
 train_readable_tbl <- process_hr_data_readable(train_raw_tbl, definitions_raw_tbl)
 test_readable_tbl  <- process_hr_data_readable(test_raw_tbl, definitions_raw_tbl)
 
 # ML Preprocessing Recipe 
 recipe_obj <- recipe(Attrition ~ ., data = train_readable_tbl) %>%
+    # remove zero variance
     step_zv(all_predictors()) %>%
-    step_num2factor(JobLevel, StockOptionLevel) %>%
+    # factor variables
+    step_mutate_at(JobLevel, StockOptionLevel, fn = factor) %>%
     prep()
 
-recipe_obj
+recipe_obj 
 
-train_tbl <- bake(recipe_obj, newdata = train_readable_tbl)
-test_tbl  <- bake(recipe_obj, newdata = test_readable_tbl)
+# bake
+train_tbl <- bake(recipe_obj, new_data = train_readable_tbl)
+test_tbl  <- bake(recipe_obj, new_data = test_readable_tbl)
+
+
 
 # 2. Models ----
+
+# initialize
+h2o.init()
+
+# load model
+automl_leader <- h2o.loadModel("R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/04_modeling/h2o_models/StackedEnsemble_BestOfFamily_1_AutoML_3_20220715_102232")
+
+automl_leader
+
+
+
+
+
