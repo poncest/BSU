159d79427cdb7896f149c16ac73a9ad9f3b86b9c
steven ponce <steven_ponce@yahoo.com>
2022-08-17 07:10:37 -0400

    08_challenges

--- R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/08_challenges/01_lime_challenge.R
+++ R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/08_challenges/01_lime_challenge.R
@@ -5,7 +5,7 @@
 # Load Libraries 
 
 library(pacman)
-p_load(h2o, recipes, readxl, tidyverse, tidyquant, lime)
+p_load(h2o, recipes, readxl, tidyverse, tidyquant, lime, glue)
 
 
 # Load Data
@@ -175,80 +175,103 @@ plot_features(explanation, ncol = 4)
 plot_explanations(explanation)
 
 
-# 4. CHALLENGE ---
-#This is a two part challenge:
+
+# 4. CHALLENGE ----
+# This is a two part challenge:
     
-# Part 1: Recreate plot_features(). Take the explanation data and use the first case to create a plot similar to the output of plot_features().
-
-#' @importFrom tools toTitleCase
-label_both_upper <- function(labels, multi_line = TRUE, sep = ': ') {
-    names(labels) <- toTitleCase(names(labels))
-    label_both(labels, multi_line, sep)
-}
-
-
-plot_features_tq <- function(explanation, ncol = 2, cases = NULL) {
-    type_pal <- c('Supports', 'Contradicts')
-    
-    if (!is.null(cases)) {
-        explanation <- explanation[explanation$case %in% cases, , drop = FALSE]
-    }
-    
-    if (nrow(explanation) == 0) stop("No explanations to plot", call. = FALSE)
-    
-    if (explanation$model_type[1] == 'regression') {
-        type_pal       <- c('Positive', 'Negative')
-        binned_feature <- grepl("=|>|<", explanation$feature_desc)
-        explanation[!binned_feature, "feature_weight"] <- as.numeric(explanation$feature_value[!binned_feature]) * explanation$feature_weight[!binned_feature]
-    }
-    
-    explanation$type <- factor(ifelse(sign(explanation$feature_weight) == 1, type_pal[1], type_pal[2]), levels = type_pal)
-    description <- paste0(explanation$case, '_', explanation[['label']])
-    desc_width  <- max(nchar(description)) + 1
-    description <- paste0(format(description, width = desc_width), explanation$feature_desc)
-    explanation$description <- factor(description, levels = description[order(abs(explanation$feature_weight))])
-    explanation$case <- factor(explanation$case, unique(explanation$case))
-    explanation$`Explanation fit` <- format(explanation$model_r2, digits = 2)
-    
-    if (explanation$model_type[1] == 'classification') {
-        explanation$probability <- format(explanation$label_prob, digits = 2)
-        explanation$label <- factor(explanation$label, unique(explanation$label[order(explanation$label_prob, decreasing = TRUE)]))
-        p <- ggplot(explanation) +
-            facet_wrap(~ case + label + probability + `Explanation fit`, labeller = label_both_upper, scales = 'free_y', ncol = ncol)
-    } else if (explanation$model_type[1] == 'regression') {
-        p <- ggplot(explanation) +
-            facet_wrap(~ case + prediction + `Explanation fit`, labeller = label_both_upper, scales = 'free_y', ncol = ncol)
-    }
-    p +
-        geom_col(aes_(~description, ~feature_weight, fill = ~type)) +
-        coord_flip() +
-        #scale_fill_manual(values = c('steelblue', 'firebrick'), drop = FALSE) +
-        scale_color_tq() +
-        
-        scale_x_discrete(labels = function(lab) substr(lab, desc_width + 1, nchar(lab))) +
-        labs(y = 'Weight', x = 'Feature', fill = '') +
-        
-        #theme_lime()
-        theme_tq()
-        explanation <- explanation[explanation$case]
-    }
-
-
-
-
+# Part 1: Recreate plot_features() ----
+# Take the explanation data and use the first case to create a plot similar to the output of plot_features().
 
 explanation %>% 
-as_tibble()
+    as_tibble()
 
 case_1 <- explanation %>%
     filter(case == 1)
 
 case_1 %>%
-    plot_features_tq()
+    plot_features()
 
-#Part 2: Recreate plot_explanations():
+
+# Transformation
+data_transformed <- case_1 %>% 
+    as_tibble() %>% 
+    mutate(
+        feature_desc = as_factor(feature_desc) %>% 
+            fct_reorder(abs(feature_weight), .desc = FALSE),
+        
+        key = ifelse(feature_weight > 0, 'Supports', 'Contradicts') %>% 
+            fct_relevel('Supports'),
+        
+        case_text  = glue('Case: {case}'),
+        label_text = glue('Label: {label}'),
+        prob_text  = glue('Probability: {round(label_prob, 2)}'),
+        r2_text    = glue('Explanation Fit: {round(model_r2, 2)}'),
+    ) %>% 
     
-#  Take the full explanation data and recreate the second plot.
+    select(feature_desc, feature_weight, key, case_text:r2_text)
+    
+# Visualization
+data_transformed %>% 
+    ggplot(aes(feature_desc, feature_weight, fill = key)) +
+    geom_bar(stat = 'identity') +
+    coord_flip() +
+    scale_fill_tq() +
+    theme_tq() +
+    labs(x = 'Feature', y = 'Weight') + 
+    theme(title = element_text(size = 9)) +
+    facet_wrap(~ case_text + label_text + prob_text + r2_text,
+               ncol = 1, scales = 'free')
+    
+
+plot_features_tq <- function(explanation, ncol) {
+    
+    # Transformation
+    data_transformed <- case_1 %>% 
+        as_tibble() %>% 
+        mutate(
+            feature_desc = as_factor(feature_desc) %>% 
+                fct_reorder(abs(feature_weight), .desc = FALSE),
+            
+            key = ifelse(feature_weight > 0, 'Supports', 'Contradicts') %>% 
+                fct_relevel('Supports'),
+            
+            case_text  = glue('Case: {case}'),
+            label_text = glue('Label: {label}'),
+            prob_text  = glue('Probability: {round(label_prob, 2)}'),
+            r2_text    = glue('Explanation Fit: {round(model_r2, 2)}'),
+        ) %>% 
+        
+        select(feature_desc, feature_weight, key, case_text:r2_text)
+    
+    # Visualization
+    data_transformed %>% 
+        ggplot(aes(feature_desc, feature_weight, fill = key)) +
+        geom_bar(stat = 'identity') +
+        coord_flip() +
+        scale_fill_tq() +
+        theme_tq() +
+        labs(x = 'Feature', y = 'Weight') + 
+        theme(title = element_text(size = 9)) +
+        facet_wrap(~ case_text + label_text + prob_text + r2_text,
+                   ncol = 1, scales = 'free')
+    
+}
+
+# testing
+plot_features_tq(explanation = explanation, ncol = 1)
+
+# ERROR
+explanation %>% 
+    filter(case %in% 1:6) %>% 
+    plot_features_tq(ncol = 3)
+
+
+
+
+
+# Part 2: Recreate plot_explanations() ----
+    
+# Take the full explanation data and recreate the second plot.
 # You will need at least the layers geom_tile() and facet_wrap().
 
 
@@ -258,3 +281,5 @@ case_1 %>%
 # HINTS:
     
 # If you do get stuck on this challenge, because this is actually a rather difficult challenge, I highly recommend checking out the library lime from Thomas Pedersens’ github page https://github.com/thomasp85/lime. All of the R code is in the folder R. 
+
+
