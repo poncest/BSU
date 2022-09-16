dcf380cc785d87c370385cd1f86d0d6d8cda8f8a
steven ponce <steven_ponce@yahoo.com>
2022-09-16 11:02:26 -0400

    05_evaluation

--- R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/05_evaluation/03_recommendation_algorithm.R
+++ R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/05_evaluation/03_recommendation_algorithm.R
@@ -6,29 +6,55 @@
 library(readxl)
 library(tidyverse)
 library(tidyquant)
-library(recipes)    # Make sure v0.1.3 or laters is installed. If not restart & install.packages("recipes") to update.
+library(recipes)    # Make sure v0.1.3 or later is installed. If not restart & install.packages("recipes") to update.
 
 
-# Data
-path_train            <- "00_Data/telco_train.xlsx"
-path_test             <- "00_Data/telco_test.xlsx"
-path_data_definitions <- "00_Data/telco_data_definitions.xlsx"
+# Load Data
+path_train            <- (here::here('./R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_data/telco_train.xlsx'))
+path_test             <- (here::here('./R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_data/telco_test.xlsx'))
+path_data_definitions <- (here::here('./R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_data/telco_data_definitions.xlsx'))
 
-train_raw_tbl       <- read_excel(path_train, sheet = 1)
-test_raw_tbl        <- read_excel(path_test, sheet = 1)
-definitions_raw_tbl <- read_excel(path_data_definitions, sheet = 1, col_names = FALSE)
+
+train_raw_tbl         <- read_excel(path_train, sheet = 1)
+test_raw_tbl          <- read_excel(path_test, sheet = 1)
+definitions_raw_tbl   <- read_excel(path_data_definitions, sheet = 1, col_names = FALSE)
+
+
 
 # Processing Pipeline
-source("00_Scripts/data_processing_pipeline_rev1.R")
+source(here::here("R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_scripts/data_processing_pipeline_rev1.R"))
 train_readable_tbl <- process_hr_data_readable(train_raw_tbl, definitions_raw_tbl)
 test_readable_tbl  <- process_hr_data_readable(test_raw_tbl, definitions_raw_tbl)
 
 
 
 # 2.0 Correlation Analysis - Machine Readable ----
-source("00_Scripts/plot_cor.R")
+source(here::here("R-Track/Course_02_DS_for_Business_Part_2/DS4B_HR201_Business_Analysis/00_scripts/plot_cor.R"))
+
 
 # 2.1 Recipes ----
+train_readable_tbl
+
+# factor names
+factor_names <- c("JobLevel", "StockOptionLevel")
+
+# Recipe - ERROR!
+# modified the final recipe from `02_data_preparation_machine_readable.R`
+recipe(Attrition ~ ., data = train_readable_tbl) %>% 
+    step_zv(all_predictors()) %>% 
+    
+    step_mutate_at(factor_names, fn = as.factor) %>% 
+    
+    step_num2factor(factor_names, levels = ) %>%     # ERROR !
+    
+    step_discretize(all_numeric(), min_unique = 1) %>%             # bins
+    
+    step_dummy(all_nominal()) %>% 
+    
+    prep()
+
+
+
 
 
 # 2.2 Correlation Visualization ----
