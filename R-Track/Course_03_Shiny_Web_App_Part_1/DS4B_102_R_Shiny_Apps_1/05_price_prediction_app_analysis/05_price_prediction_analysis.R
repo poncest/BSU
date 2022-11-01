89f88a50486258cc330f837a100701edc92c7194
steven ponce <steven_ponce@yahoo.com>
2022-11-01 15:20:49 -0400

    05_price_prediction_app_analysis

--- R-Track/Course_03_Shiny_Web_App_Part_1/DS4B_102_R_Shiny_Apps_1/05_price_prediction_app_analysis/05_price_prediction_analysis.R
+++ R-Track/Course_03_Shiny_Web_App_Part_1/DS4B_102_R_Shiny_Apps_1/05_price_prediction_app_analysis/05_price_prediction_analysis.R
@@ -30,7 +30,6 @@ dbDisconnect(con)
 
 
 # 2.0 PREPROCESS DATA ----
-
 train_tbl <- bikes_tbl %>%
     
     # 2.1 Separate Description Column ----
@@ -158,20 +157,100 @@ separate_bike_description <- function(data, keep_description_column = TRUE,  app
     return(output_tbl)
 }
 
-# Testing separate_bike_description()
-bikes_tbl %>% separate_bike_description()
-bikes_tbl %>% separate_bike_description(append = FALSE)
-bikes_tbl %>% separate_bike_description(keep_description_column = FALSE)   #????
 
 
 # 4.2 separate_bike_model() ----
- 
+
+data <- bikes_tbl
+
+separate_bike_model <- function(data, keep_model_column = TRUE,  append = TRUE) {
+    
+    # append = False
+    if (! append) {
+        data <- data %>% select(model)
+    }
+    
+    # Fix typo
+    output_tbl <- data %>% 
+        mutate(model = case_when(
+        model == "CAAD Disc Ultegra"          ~ "CAAD12 Disc Ultegra",
+        model == "Syapse Carbon Tiagra"       ~ "Synapse Carbon Tiagra",
+        model == "Supersix Evo Hi-Mod Utegra" ~ "Supersix Evo Hi-Mod Ultegra",
+        TRUE ~ model
+    )) %>% 
+        
+        # separate using spaces
+        separate(col     = model, 
+                 into    = str_c("model_", 1:7), 
+                 sep     = " ", 
+                 remove  = FALSE, 
+                 fill    = "right") %>%
+        
+        # creating a "base" feature
+        mutate(model_base = case_when(
+            
+            # Fix Supersix Evo
+            str_detect(str_to_lower(model_1), "supersix") ~ str_c(model_1, model_2, sep = " "),
+            
+            # Fix Fat CAAD bikes
+            str_detect(str_to_lower(model_1), "fat") ~ str_c(model_1, model_2, sep = " "),
+            
+            # Fix Beast of the East
+            str_detect(str_to_lower(model_1), "beast") ~ str_c(model_1, model_2, model_3, model_4, sep = " "),
+            
+            # Fix Bad Habit
+            str_detect(str_to_lower(model_1), "bad") ~ str_c(model_1, model_2, sep = " "),
+            
+            # Fix Scalpel 29
+            str_detect(str_to_lower(model_2), "29") ~ str_c(model_1, model_2, sep = " "),
+            
+            # catch all
+            TRUE ~ model_1)
+        ) %>%
+        
+        # Get "tier" feature
+        mutate(model_tier = model %>% str_replace(model_base, replacement = "") %>% str_trim()) %>%
+        
+        # Remove unnecessary columns
+        select(-matches("model_[0-9]")) %>%
+        
+        # Create Flags
+        mutate(
+            black     = model_tier %>% str_to_lower() %>% str_detect("black") %>% as.numeric(),
+            hi_mod    = model_tier %>% str_to_lower() %>% str_detect("hi-mod") %>% as.numeric(),
+            team      = model_tier %>% str_to_lower() %>% str_detect("team") %>% as.numeric(),
+            red       = model_tier %>% str_to_lower() %>% str_detect("red") %>% as.numeric(),
+            ultegra   = model_tier %>% str_to_lower() %>% str_detect("ultegra") %>% as.numeric(),
+            dura_ace  = model_tier %>% str_to_lower() %>% str_detect("dura ace") %>% as.numeric(),
+            disc      = model_tier %>% str_to_lower() %>% str_detect("disc") %>% as.numeric()
+        )
+    
+
+    # keep_model_column = False
+    if (! keep_model_column) output_tbl <- output_tbl %>% select(-model)
+    
+    return(output_tbl)
+}
+
+
+
 # 4.3 Test Functions ----
+
+# Testing separate_bike_description()
+bikes_tbl %>% separate_bike_description()
+bikes_tbl %>% separate_bike_description(append = FALSE)
+bikes_tbl %>% separate_bike_description(keep_description_column = FALSE)   
+
+# Testing separate_bike_model()
+bikes_tbl %>% separate_bike_model()
+bikes_tbl %>% separate_bike_model(append = FALSE)
+bikes_tbl %>% separate_bike_model(keep_model_column = FALSE)  
+
 
 # 4.4 Save Functions ----
 
 dump(c("separate_bike_model", "separate_bike_description"), 
-     file = "00_scripts/01_process_data.R")
+     file = "R-Track/Course_03_Shiny_Web_App_Part_1/DS4B_102_R_Shiny_Apps_1/00_scripts/02_process_data.R")
 
 
 # 5.0 USER INPUT & PREDICTION ----
