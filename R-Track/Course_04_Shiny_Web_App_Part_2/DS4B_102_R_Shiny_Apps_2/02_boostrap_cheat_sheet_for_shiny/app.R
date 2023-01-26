a375d42fe7c548853f287f3a3c24760e442def63
steven ponce <steven_ponce@yahoo.com>
2023-01-26 05:18:58 -0500

    02_boostrap_cheat_sheet_for_shiny

--- R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/02_boostrap_cheat_sheet_for_shiny/app.R
+++ R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/02_boostrap_cheat_sheet_for_shiny/app.R
@@ -1,14 +1,46 @@
 # BOOSTRAP CHEAT SHEET FOR SHINY ----
-#DS$B 202-R ----
+#DS4B 202-R ----
 
+
+# LIBRARIES ----
 library(pacman)
 p_load(shiny, tidyverse, plotly)
 
 
-ui <- shiny::fluidPage(title = "Bootstrap Cheat Sheet for Shiny")
+# USER INTERFACE ----
+ui <- shiny::fluidPage(
+    title = "Bootstrap Cheat Sheet for Shiny",
+    
+    div(
+        class = "container",
+        id    = "page",
+        
+        ## HEARDER ----
+        # https://getbootstrap.com/docs/3.3/components/#page-header
+        
+        # shiny format
+        h1(class = "page-header", "Bootstrap Cheat Sheet", tags$small("For Shiny")),
+        
+        p(class = "lead",  
+          "This cheat sheet is the first of the", 
+          "Expert Shiny Application Development Course",
+          "by Business Science")
+        
+        
+        # HTML format
+        # HTML(
+        #     '<h1 class = "page-header">
+        #         Bootstrap Cheat Sheet
+        #         <small>For Shiny</small>
+        #         </h1>)'
+        # )
+        
+    )
+    
+    )
 
 
-
+# SERVER ----
 server <- function(input, output, session) {
      
 }
