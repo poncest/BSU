8dff6236c65d02e32d880d7195a816e42cf1b557
steven ponce <steven_ponce@yahoo.com>
2023-03-17 06:49:15 -0400

    04_dynamic_ui_training

---
+++ R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/04_dynamic_ui_training/01_dynamic_ui_training.R
@@ -0,0 +1,125 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# DYNAMIC UI TRAINING -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - Render UI a single UI component
+# - Render Multiple UI Components
+# - Store user information & react to adding and deleting
+
+# LIBRARIES ----
+# Shiny
+library(shiny)
+library(shinyWidgets)
+library(shinythemes)
+
+# Core
+library(tidyverse)
+
+# CUSTOM FUNCTIONS ----
+info_card <- function(title, value, sub_value,
+                      main_icon = "chart-line", sub_icon = "arrow-up",
+                      bg_color = "default", text_color = "default", sub_text_color = "success") {
+    
+    div(
+        class = "panel panel-default",
+        div(
+            class = str_glue("panel-body bg-{bg_color} text-{text_color}"),
+            p(class = "pull-right", icon(class = "fa-3x", main_icon)),
+            h4(title),
+            h5(value),
+            p(
+                class = str_glue("text-{sub_text_color}"),
+                icon(sub_icon),
+                tags$small(sub_value)
+            )
+        )
+    )
+    
+}
+
+
+
+ui <- fixedPage(
+    title = "Dynamic UI Components",
+    
+    theme = shinytheme("flatly"),
+    
+    h1(class = "page-header", "Dynamic UI Training"),
+    
+    # 1.0 SINGLE ITEM ----
+    h2("Intro to UI Rendering - Single Item UI Rendering"),
+    div(
+        class = "container",
+        id = "single_item",
+        column(
+            width = 4,
+            class = "well",
+            
+            selectInput(inputId  = "sales_metrics_1", 
+                        label    = "Sales Metrics", 
+                        choices  = c("Revenue", "Profit", "Qty"),
+                        selected = "Revenue"),
+            
+            selectInput(inputId  = "sales_region_1", 
+                        label    = "Sales Region", 
+                        choices  = c("North", "South", "East", "West"),
+                        selected = "North"),
+            
+            numericInput(inputId = "metric_value_1", 
+                         label   = "Metric Percent", 
+                         value   = 0.5),
+            
+            actionButton(inputId = "created_card_1", 
+                         label   = "Create Card")
+            
+        ),
+        column(
+            width = 8,
+            column(
+                width = 3,
+                info_card(title = "Revenue", value = "North", sub_value = "20%")
+                )
+        )
+        
+    ),
+    
+    # 2.0 MULTI ITEM -----
+    h2("Multi-UI Rendering with User Data Storage"),
+    div(
+        class = "container",
+        id = "multi_item",
+        p("Multi item placeholder...")
+        
+    ),
+    
+    
+    div(style = "height:400px;")
+    
+    
+)
+
+# SERVER ----
+server <- function(input, output, session) {
+    
+    # 1.0 Single Item -----
+    
+    
+    # 2.0 Multi Item -----
+    
+    # 2.1 Reactive Values & Storing User Input ----
+    
+    
+    # 2.2 Rendering Multiple Items (tagList & map) ----
+    
+    
+    # 2.3 Rendering Inputs Items ----
+    
+    
+    # 2.4 Delete Item ----
+    
+    
+}
+
+shinyApp(ui, server)
