0fe03359d190a0dec6618c58449eb3836af6948b
poncest <steven_ponce@yahoo.com>
2023-05-08 05:00:50 -0400

    01_stock_analyzer_app

--- R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/01_stock_analyzer_app/09_stock_analyzer_persistent_data.R
+++ R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/01_stock_analyzer_app/09_stock_analyzer_persistent_data.R
@@ -56,7 +56,7 @@ ui <- tagList(
 server <- function(input, output, session) {
     
     # 0.0 READ DATA ----
-    user_base_tbl <- read_rds(here::here("R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_data_local/user_base_tbl.rds"))
+    user_base_tbl <<- read_rds(here::here("R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_data_local/user_base_tbl.rds"))
     
     # 
     
