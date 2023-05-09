692d027e1c0c26524a5a841c1fa18b107c63b315
poncest <steven_ponce@yahoo.com>
2023-05-09 05:14:41 -0400

    01_stock_analyzer_app

--- R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_analysis/crud_workflow.R
+++ R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_analysis/crud_workflow.R
@@ -3,3 +3,8 @@
 # STOCK ANALYZER APP - PERSISTENT DATA -----
 # Version 1
 # CRUD WORKFLOW
+
+
+user_base_tbl <<- read_rds(here::here("R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_data_local/user_base_tbl.rds"))
+
+user_base_tbl[user_base_tbl$user == "user1", ]["last_symbol"] <- "MA"
