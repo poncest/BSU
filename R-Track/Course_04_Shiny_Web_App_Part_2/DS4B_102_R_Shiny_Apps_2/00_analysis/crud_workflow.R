ce4175d22c9d1120ae8144d3628baa7b238861de
poncest <steven_ponce@yahoo.com>
2023-05-10 05:22:08 -0400

    01_stock_analyzer_app

--- R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_analysis/crud_workflow.R
+++ R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_analysis/crud_workflow.R
@@ -7,4 +7,27 @@
 
 user_base_tbl <<- read_rds(here::here("R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_data_local/user_base_tbl.rds"))
 
-user_base_tbl[user_base_tbl$user == "user1", ]["last_symbol"] <- "MA"
+user_base_tbl[user_base_tbl$user == "user1", ][["last_symbol"]] <- "MA"
+
+user_base_tbl[user_base_tbl$user == "user1", ][["favorites"]] <- list(c("AAPL", "GOOG", "NFLX", "MA")) 
+
+user_base_tbl[user_base_tbl$user == "user1", ][["user_settings"]]
+
+user_settings <- tibble(
+    moving_avg_short = 15,
+    moving_avg_long  = 500,
+    time_window      = 365
+    )
+
+user_settings 
+
+user_base_tbl[user_base_tbl$user == "user1", ][["user_settings"]] <- list(user_settings)
+
+
+write_rds(user_base_tbl, path = here:here("R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_data_local/user_base_tbl.rds"))
+
+data <- read_rds(file = "R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_data_local/user_base_tbl.rds")
+
+data %>% 
+    filter(user == "user1") %>% 
+    pull(user_settings)
