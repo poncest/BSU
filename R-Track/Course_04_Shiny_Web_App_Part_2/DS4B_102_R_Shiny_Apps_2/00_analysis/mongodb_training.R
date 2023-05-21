8a3e2d438d647c3700313be7a002117ffe0e0284
poncest <steven_ponce@yahoo.com>
2023-05-21 09:23:56 -0400

    00_analysis

---
+++ R-Track/Course_04_Shiny_Web_App_Part_2/DS4B_102_R_Shiny_Apps_2/00_analysis/mongodb_training.R
@@ -0,0 +1,144 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# MONGO DB TRAINING -----
+# Version 1
+
+# LIBRARIES ----
+library(mongolite) # Resource: https://jeroen.github.io/mongolite/
+library(jsonlite)
+
+library(config)
+
+library(tidyverse)
+library(lubridate)
+
+
+# 1.0 CONNECTION TO REMOTE MONGODB ---- 
+
+# Setup config Package & database YAML
+
+
+# Connect to MongoDB Atlas Cloud Database
+
+
+# 2.0 ADD DATA ----
+
+# Connect to collection
+
+
+# Adding data
+
+
+# 3.0 QUERYING DATA ----
+
+
+
+# 4.0 MODIFYING A COLLECTION ----
+
+new_car_tbl <- tibble(
+    model = "Ford F150",
+    mpg   = 9.8,
+    cyl   = 8,
+    disp  = 275.8,
+    hp    = 180,
+    drat  = 3.07,
+    wt    = 7.85,
+    qsec  = 23.45,
+    vs    = 0,
+    am    = 1,
+    gear  = 4,
+    carb  = 3
+)
+
+new_car_tbl
+
+# 4.1 Insert New Record
+
+
+# 4.2 Change a Record
+
+
+# 4.3 Remove a record
+
+
+# 4.4 Remove entire table (be careful)
+
+
+
+
+# 5.0 NESTED STRUCTURES ----
+
+mongo_connection <- mongo_connect(
+    database   = "stock_analyzer",
+    collection = "user_base"
+)
+
+mongo_connection$drop()
+mongo_connection$count()
+
+user_base_tbl <- tibble(
+    user           = c("user1", "user2"),
+    password       = c("pass1", "pass2"), 
+    permissions    = c("admin", "standard"),
+    name           = c("User One", "User Two"),
+    favorites      = list(c("AAPL", "GOOG", "NFLX"), c("MA", "V", "FB")),
+    last_symbol    = c("GOOG", "NFLX"),
+    user_settings  = list(tibble(moving_avg_short = 20, moving_avg_long = 50, time_window = 180), 
+                          tibble(moving_avg_short = 30, moving_avg_long = 90, time_window = 365)),
+    account_created = c(ymd_hms("2019-05-12 12:31:09"), ymd_hms("2019-06-04 06:18:02"))
+) 
+
+# Converting to JSON
+
+
+# Adding nested structure to mongodb
+
+
+# Retrieve - Preserves nested structure and format
+
+
+
+# 6.0 STOCK ANALYZER APP - CRUD WORKFLOW -----
+
+# Create new collection
+mongo_connection <- mongo_connect(
+    database   = "stock_analyzer",
+    collection = "user_base"
+)
+
+# 6.1 Add User Data ----
+
+
+
+# 6.2 Get User Data ----
+# read_user_base <- function() {
+#     user_base_tbl <<- read_rds(path = "00_data_local/user_base_tbl.rds")
+# }
+
+
+
+# 6.3 What shinyauthr does... ----
+
+
+
+# 6.5 Update Mongo ----
+
+# update_and_write_user_base <- function(user_name, column_name, assign_input) {
+#     user_base_tbl[user_base_tbl$user == user_name, ][[column_name]] <<- assign_input
+#     write_rds(user_base_tbl, path = "00_data_local/user_base_tbl.rds")
+# }
+
+
+
+# Before update
+
+
+# After update
+
+
+# 7.0 Save Functions ----
+
+dump(c("mongo_connect", "mongo_get_user_base", "mongo_update_user_record"), 
+     file = "00_scripts/crud_operations_mongodb.R", append = FALSE)
+
+
